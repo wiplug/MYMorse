de346ffd770a90e380bfd78b68090d3ca63dee6e
Maxime Feron <m8feron@enib.fr>
2013-04-11 19:49:55 +0200

    jhtgjyhfjhfjhf

--- src/net/enib/MainActivity.java
+++ src/net/enib/MainActivity.java
@@ -1,7 +1,15 @@
 package net.enib;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d("Essai", "Essai");
+	}
 }
